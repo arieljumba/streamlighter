fcd096e2d9c6baeab6f6df50f8ac8d082953d8ae
Ariel Jumba <arieljumba5@gmail.com>
2022-05-28 14:49:56 +0300

    update1

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app = MultiApp()
 st.set_page_config(layout='wide')
 with open('style.css') as f:
     st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
-app.add_app("Stock Price Prediction", timeseries.app)
+app.add_app("timeseries, timeseries.app)
 
 # The main app
 app.run()
